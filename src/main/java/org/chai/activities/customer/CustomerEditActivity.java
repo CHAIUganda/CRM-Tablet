1d942472830f8f631b04af36e05c001cc1b62209
Victor Kakama <vkakama@gmail.com>
2014-10-23 10:08:59 +0300

    created a customer profile edit activity

---
+++ src/main/java/org/chai/activities/customer/CustomerEditActivity.java
@@ -0,0 +1,15 @@
+package org.chai.activities.customer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.chai.R;
+
+/**
+ * Created by victor on 10/23/14.
+ */
+public class CustomerEditActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_edit_form);
+    }
+}
