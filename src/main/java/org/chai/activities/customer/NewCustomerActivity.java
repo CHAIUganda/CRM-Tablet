41f015a56f44fd52f7ef7a4f50cdba0abe6e8de9
Victor Kakama <vkakama@gmail.com>
2014-10-27 17:11:33 +0300

    change subcounty field to be a spinner

--- src/main/java/org/chai/activities/customer/NewCustomerActivity.java
+++ src/main/java/org/chai/activities/customer/NewCustomerActivity.java
@@ -1,19 +1,83 @@
 package org.chai.activities.customer;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
 import org.chai.R;
+import org.chai.adapter.SubcountyArrayAdapter;
+import org.chai.model.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by victor on 10/20/14.
  */
 public class NewCustomerActivity extends Activity {
+    private Spinner subcountySpinner;
+    private SubcountyArrayAdapter adapter;
+    private SQLiteDatabase db;
+    private DaoMaster daoMaster;
+    private DaoSession daoSession;
+    private CustomerDao customerDao;
+    private SubcountyDao subcountyDao;
+
+    private Subcounty[] subcounties;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_form);
+        initialiseGreenDao();
+        try{
+            List<Subcounty> subcountyData = subcountyDao.loadAll();
+            subcounties = subcountyData.toArray(new Subcounty[subcountyData.size()]);
+            subcountySpinner = (Spinner)findViewById(R.id.details_subcounty);
+
+            adapter = new SubcountyArrayAdapter(this,android.R.layout.simple_spinner_item,subcounties);
+            subcountySpinner.setAdapter(adapter);
+
+            subcountySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+//                            Subcounty subcounty = adapter.getItem(position);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+
+            Button addNewCustomerBtn = (Button)findViewById(R.id.menu_add_new_customer);
+            addNewCustomerBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Subcounty subcounty = ((Subcounty)subcountySpinner.getSelectedItem());
+                    Toast.makeText(getApplicationContext(), "Subcounty:"+subcounty.getName() , Toast.LENGTH_LONG).show();
+                }
+            });
+        }catch (Exception ex){
+            //
+        }
+    }
+
+    private void initialiseGreenDao(){
+        try{
+            DaoMaster.DevOpenHelper helper =new  DaoMaster.DevOpenHelper(this,"chai-crm-db",null);
+            db = helper.getWritableDatabase();
+            daoMaster = new DaoMaster(db);
+            daoSession = daoMaster.newSession();
+            customerDao = daoSession.getCustomerDao();
+            subcountyDao = daoSession.getSubcountyDao();
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Error initialising Database:" + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -25,8 +89,35 @@ public class NewCustomerActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem menuItem){
         switch (menuItem.getItemId()){
             case R.id.menu_add_new_customer:
+                saveNewCustomer();
             default:
                 return super.onOptionsItemSelected(menuItem);
         }
+    }
+
+    private void saveNewCustomer() {
+        Customer customer = new Customer(null);
+        customer.setSysid(UUID.randomUUID().toString());
+        customer.setOutletName(((EditText) findViewById(R.id.detailsname)).getText().toString());
+        customer.setKeyWholeSalerContact(((EditText)findViewById(R.id.address)).getText().toString());
+        customer.setTenureStartDate(new Date());
+        customer.setTenureEndDate(new Date());
+        customer.setOutletType(((EditText)findViewById(R.id.details_outlet_type)).getText().toString());
+        customer.setOutletSize(((EditText)findViewById(R.id.details_size)).getText().toString());
+        customer.setSplit(((EditText)findViewById(R.id.details_split)).getText().toString());
+        customer.setOpeningHours(((EditText)findViewById(R.id.detail_opening_hrs)).getText().toString());
+        customer.setMajoritySourceOfSupply(((EditText)findViewById(R.id.details_sources_of_supply)).getText().toString());
+        customer.setKeyWholeSalerName(((EditText)findViewById(R.id.details_key_wholesaler_name)).getText().toString());
+        customer.setKeyWholeSalerContact(((EditText)findViewById(R.id.details_key_wholesaler_contact)).getText().toString());
+        customer.setBuildingStructure(((EditText)findViewById(R.id.details_building_structure)).getText().toString());
+        customer.setEquipment(((EditText)findViewById(R.id.details_equipment)).getText().toString());
+        customer.setDescriptionOfOutletLocation(((EditText)findViewById(R.id.details_desc_location)).getText().toString());
+        customer.setNumberOfEmployees(Integer.parseInt(((EditText)findViewById(R.id.details_number_of_employees)).getText().toString()));
+        customer.setNumberOfBranches(Integer.parseInt(((EditText)findViewById(R.id.details_number_of_branches)).getText().toString()));
+        customer.setNumberOfCustomersPerDay(Integer.parseInt(((EditText)findViewById(R.id.details_num_customers_per_day)).getText().toString()));
+        customer.setNumberOfProducts(Integer.parseInt(((EditText)findViewById(R.id.details_num_products)).getText().toString()));
+        customer.setRestockFrequency(Integer.parseInt(((EditText)findViewById(R.id.details_restock_frequency)).getText().toString()));
+        customer.setTurnOver(Double.parseDouble(((EditText)findViewById(R.id.details_turn_over)).getText().toString()));
+//        customer.setSubcounty();
     }
 }
--- src/main/java/org/chai/adapter/SubcountyArrayAdapter.java
+++ src/main/java/org/chai/adapter/SubcountyArrayAdapter.java
@@ -1,7 +1,53 @@
 package org.chai.adapter;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import org.chai.model.Subcounty;
+
 /**
  * Created by victor on 10/27/14.
  */
-public class SubcountyArrayAdapter {
+public class SubcountyArrayAdapter extends ArrayAdapter<Subcounty> {
+
+    private Context context;
+    private Subcounty[] subcounties;
+
+    public SubcountyArrayAdapter(Context context, int textViewResourceId,Subcounty[] subcounties) {
+        super(context, textViewResourceId,subcounties);
+        this.context = context;
+        this.subcounties = subcounties;
+    }
+
+    public int getCount(){
+        return subcounties.length;
+    }
+
+    public Subcounty getItem(int position){
+        return subcounties[position];
+    }
+
+    public long getItemId(int position){
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView label = new TextView(context);
+        label.setTextColor(Color.BLACK);
+        label.setText(subcounties[position].getName());
+        return label;
+    }
+    @Override
+    public View getDropDownView(int position, View convertView,ViewGroup parent) {
+        TextView label = new TextView(context);
+        label.setTextColor(Color.BLACK);
+        label.setText(subcounties[position].getName());
+
+        return label;
+    }
+
 }
