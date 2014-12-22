d17f69b934a0884ea4cc496b108cd779cfc78ca1
Victor Kakama <vkakama@gmail.com>
2014-12-22 18:22:03 +0300

    added order form

--- src/main/java/org/chai/activities/tasks/TakeOrderActivity.java
+++ src/main/java/org/chai/activities/tasks/TakeOrderActivity.java
@@ -1,9 +1,6 @@
 package org.chai.activities.tasks;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -12,48 +9,50 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 import org.chai.R;
-import android.widget.TableRow.LayoutParams;
-import org.chai.model.*;
+import org.chai.model.Customer;
+import org.chai.model.CustomerDao;
+import org.chai.model.DaoMaster;
+import org.chai.model.DaoSession;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
- * Created by victor on 10/26/14.
+ * Created by victor on 12/22/14.
  */
-public class CommercialFormActivity extends Activity {
+public class TakeOrderActivity extends Activity {
 
     private SQLiteDatabase db;
     private DaoMaster daoMaster;
     private DaoSession daoSession;
-    private TaskDao taskDao;
-    private SaleDao saleDao;
-
+    private CustomerDao customerDao;
 
     private TableLayout tableLayout;
     private List<Spinner> spinnerList;
     private List<EditText> quantityFields;
     private List<EditText> priceFields;
-    private Task callDataTask;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sales_form);
+        setContentView(R.layout.order_form);
         initialiseGreenDao();
-        tableLayout = (TableLayout)findViewById(R.id.sales_table);
+
+        tableLayout = (TableLayout)findViewById(R.id.orders_table);
         spinnerList = new ArrayList<Spinner>();
         quantityFields = new ArrayList<EditText>();
         priceFields = new ArrayList<EditText>();
-        Bundle bundle = getIntent().getExtras();
-        Long taskId = bundle.getLong("taskId");
-        callDataTask = taskDao.loadDeep(taskId);
 
-        spinnerList.add((Spinner) findViewById(R.id.sales_product));
-        quantityFields.add((EditText) findViewById(R.id.sales_quantity));
-        priceFields.add((EditText) findViewById(R.id.sales_price));
+        spinnerList.add((Spinner) findViewById(R.id.order_product));
+        quantityFields.add((EditText) findViewById(R.id.order_quantity));
+        priceFields.add((EditText) findViewById(R.id.order_price));
 
-        Button addButton = (Button)findViewById(R.id.sales_add_more);
+        List<Customer> customersList = customerDao.loadAll();
+        String[] customers = getCustomerName(customersList);
+        AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.sales_auto_complete_textview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,customers);
+        textView.setAdapter(adapter);
+
+        Button addButton = (Button)findViewById(R.id.order_add_more);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,23 +60,14 @@ public class CommercialFormActivity extends Activity {
             }
         });
 
-        Button takeOrder = (Button)findViewById(R.id.sales_take_order);
-        takeOrder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),TakeOrderActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
 
-        Button saveBtn = (Button) findViewById(R.id.sales_save_sale);
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                submitSale();
-            }
-        });
-
+    private String[] getCustomerName(List<Customer> customers) {
+        String[] customerArray = new String[customers.size()];
+        for(int i=0;i<customers.size();++i){
+            customerArray[i] = customers.get(i).getOutletName();
+        }
+        return customerArray;
     }
 
     private void initialiseGreenDao() {
@@ -86,21 +76,19 @@ public class CommercialFormActivity extends Activity {
             db = helper.getWritableDatabase();
             daoMaster = new DaoMaster(db);
             daoSession = daoMaster.newSession();
-            taskDao = daoSession.getTaskDao();
-            saleDao = daoSession.getSaleDao();
+            customerDao = daoSession.getCustomerDao();
         } catch (Exception ex) {
             Log.d("Error=====================================", ex.getLocalizedMessage());
             Toast.makeText(getApplicationContext(), "Error initialising Database:" + ex.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
-
     private void addRowToTable(){
         TableRow tableRow = new TableRow(this);
-        tableRow.setLayoutParams(new LayoutParams( LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        tableRow.setLayoutParams(new TableRow.LayoutParams( TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
 
         Spinner spinner  = new Spinner(this);
-        LayoutParams spinnerParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams spinnerParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
         spinner.setLayoutParams(spinnerParams);
         ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
                 android.R.layout.simple_spinner_dropdown_item,
@@ -110,7 +98,7 @@ public class CommercialFormActivity extends Activity {
 
         tableRow.addView(spinner);
 
-        LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
         params.setMargins(10,10,10,10);
         EditText quantityView =(EditText)getLayoutInflater().inflate(R.layout.edit_text_style,null);
         quantityView.setTextColor(Color.BLACK);
@@ -138,7 +126,7 @@ public class CommercialFormActivity extends Activity {
         });
         tableRow.addView(deleteBtn);
 
-        tableLayout.addView(tableRow, new TableLayout.LayoutParams( LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        tableLayout.addView(tableRow, new TableLayout.LayoutParams( TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
     }
 
     private void removeRow() {
@@ -153,17 +141,6 @@ public class CommercialFormActivity extends Activity {
         }
     }
 
-    private void submitSale() {
-        for (int i = 0; i < spinnerList.size(); ++i) {
-            Sale sale = new Sale(null);
-            sale.setDateOfSale(new Date());
-            sale.setQuantity(Integer.parseInt(quantityFields.get(i).getText().toString()));
-            sale.setSalePrice(Integer.parseInt(priceFields.get(i).getText().toString()));
-            sale.setOrderId(callDataTask.getId());
-//            sale.setProductId();
-            saleDao.insert(sale);
-        }
-    }
 
 
 }
