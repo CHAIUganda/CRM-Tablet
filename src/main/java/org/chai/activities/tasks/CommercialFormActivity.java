7125ab6f19b982ea3bdbc66257f43810e13b4738
Victor Kakama <vkakama@gmail.com>
2014-12-22 17:15:13 +0300

    added submit function for saving sales

--- src/main/java/org/chai/activities/tasks/CommercialFormActivity.java
+++ src/main/java/org/chai/activities/tasks/CommercialFormActivity.java
@@ -3,29 +3,56 @@ package org.chai.activities.tasks;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 import org.chai.R;
 import android.widget.TableRow.LayoutParams;
+import org.chai.model.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by victor on 10/26/14.
  */
 public class CommercialFormActivity extends Activity {
-    static final int DATE_PICKER_ID = 1111;
-    private EditText dateEditTxt;
-    private DatePickerDialog datePickerDialog;
-    private String initialDate;
-    private String initialMonth;
-    private String initialYear;
+
+    private SQLiteDatabase db;
+    private DaoMaster daoMaster;
+    private DaoSession daoSession;
+    private TaskDao taskDao;
+    private SaleDao saleDao;
+
+
     private TableLayout tableLayout;
+    private List<Spinner> spinnerList;
+    private List<EditText> quantityFields;
+    private List<EditText> priceFields;
+    private Task callDataTask;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sales_form);
+        initialiseGreenDao();
         tableLayout = (TableLayout)findViewById(R.id.sales_table);
+        spinnerList = new ArrayList<Spinner>();
+        quantityFields = new ArrayList<EditText>();
+        priceFields = new ArrayList<EditText>();
+        Bundle bundle = getIntent().getExtras();
+        Long taskId = bundle.getLong("taskId");
+        callDataTask = taskDao.loadDeep(taskId);
+
+        spinnerList.add((Spinner) findViewById(R.id.sales_product));
+        quantityFields.add((EditText) findViewById(R.id.sales_quantity));
+        priceFields.add((EditText) findViewById(R.id.sales_price));
+
         Button addButton = (Button)findViewById(R.id.sales_add_more);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,17 +61,30 @@ public class CommercialFormActivity extends Activity {
             }
         });
 
+        Button saveBtn = (Button) findViewById(R.id.sales_save_sale);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitSale();
+            }
+        });
+
     }
 
-    private class PickDate implements DatePickerDialog.OnDateSetListener{
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth) {
-            view.updateDate(year, monthOfYear, dayOfMonth);
-            dateEditTxt.setText(monthOfYear+"/"+dayOfMonth+"/"+year);
-            datePickerDialog.hide();
+    private void initialiseGreenDao() {
+        try {
+            DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "chai-crm-db", null);
+            db = helper.getWritableDatabase();
+            daoMaster = new DaoMaster(db);
+            daoSession = daoMaster.newSession();
+            taskDao = daoSession.getTaskDao();
+            saleDao = daoSession.getSaleDao();
+        } catch (Exception ex) {
+            Log.d("Error=====================================", ex.getLocalizedMessage());
+            Toast.makeText(getApplicationContext(), "Error initialising Database:" + ex.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
+
 
     private void addRowToTable(){
         TableRow tableRow = new TableRow(this);
@@ -84,6 +124,7 @@ public class CommercialFormActivity extends Activity {
                 ViewGroup container = ((ViewGroup)row.getParent());
                 container.removeView(row);
                 container.invalidate();
+                removeRow();
             }
         });
         tableRow.addView(deleteBtn);
@@ -91,6 +132,29 @@ public class CommercialFormActivity extends Activity {
         tableLayout.addView(tableRow, new TableLayout.LayoutParams( LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
     }
 
+    private void removeRow() {
+        if (!spinnerList.isEmpty()) {
+            spinnerList.remove(spinnerList.size() - 1);
+        }
+        if (!quantityFields.isEmpty()) {
+            quantityFields.remove(quantityFields.size() - 1);
+        }
+        if (!priceFields.isEmpty()) {
+            priceFields.remove(priceFields.size() - 1);
+        }
+    }
+
+    private void submitSale() {
+        for (int i = 0; i < spinnerList.size(); ++i) {
+            Sale sale = new Sale(null);
+            sale.setDateOfSale(new Date());
+            sale.setQuantity(Integer.parseInt(quantityFields.get(i).getText().toString()));
+            sale.setSalePrice(Integer.parseInt(priceFields.get(i).getText().toString()));
+            sale.setOrderId(callDataTask.getId());
+//            sale.setProductId();
+            saleDao.insert(sale);
+        }
+    }
 
 
 }
