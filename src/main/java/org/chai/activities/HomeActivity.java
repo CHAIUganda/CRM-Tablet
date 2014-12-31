573ac27c5c1183f4b9e26ab338e10fad77180b95
Victor Kakama <vkakama@gmail.com>
2014-12-31 18:25:26 +0300

    remove product menu on the drawer, we dont need it since we are downloading products from the server

--- src/main/java/org/chai/activities/HomeActivity.java
+++ src/main/java/org/chai/activities/HomeActivity.java
@@ -66,7 +66,6 @@ public class HomeActivity extends FragmentActivity{
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[1],navMenuIcons.getResourceId(1,-1)));
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[2],navMenuIcons.getResourceId(2,-1)));
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[3],navMenuIcons.getResourceId(3,-1)));
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4],navMenuIcons.getResourceId(4,-1)));
 
 
         navMenuIcons.recycle();
