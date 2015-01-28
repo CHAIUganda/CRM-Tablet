45e6efef4075b40b2c8c1ee7cbe27c7f053d7f91
Victor Kakama <vkakama@gmail.com>
2015-01-28 15:10:05 +0300

    adjust detailer form to use base class

--- src/main/java/org/chai/activities/tasks/DetailersActivity.java
+++ src/main/java/org/chai/activities/tasks/DetailersActivity.java
@@ -1,56 +1,30 @@
 package org.chai.activities.tasks;
 
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
-import android.util.Log;
 import android.view.*;
 import android.widget.*;
 import de.greenrobot.dao.query.Query;
 import de.greenrobot.dao.query.WhereCondition;
 import org.chai.R;
 import org.chai.activities.HomeActivity;
-import org.chai.adapter.DistrictArrayAdapter;
 import org.chai.model.*;
-import org.chai.util.GPSTracker;
 import org.chai.util.Utils;
 
-import java.util.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by victor on 10/30/14.
  */
-public class DetailersActivity extends Fragment {
+public class DetailersActivity extends BaseDetailerFragment {
 
-    private EditText dateEditTxt;
-    private DatePickerDialog datePickerDialog;
-    private String initialDate;
-    private String initialMonth;
-    private String initialYear;
-
-    private GPSTracker gpsTracker;
-    private double capturedLatitude;
-    private double capturedLongitude;
-
-    private SQLiteDatabase db;
-    private DaoMaster daoMaster;
-    private DaoSession daoSession;
-    private VillageDao villageDao;
-    private TaskDao taskDao;
-    private DetailerCallDao detailerCallDao;
-
-    private TextView subcountyTxt;
-    private DistrictArrayAdapter adapter;
-
-    private DetailerCall detailerCallInstance;
     private Task callDataTask;
     private Button pointOfSalesOptionsButton;
     private CharSequence[] pointOfSalesOptions;
@@ -58,7 +32,7 @@ public class DetailersActivity extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.detailers_form,container, false);
+        final View view = inflater.inflate(R.layout.detailers_form,container, false);
         initialiseGreenDao();
         Bundle bundle = getArguments();
         String callId = bundle.getString("callId");
@@ -87,6 +61,7 @@ public class DetailersActivity extends Fragment {
         manageDoyouStockZincResponses(view);
         manageHowDidyouHearOtherOption(view);
         manageHaveYouHeardAboutDiarheaTreatment(view);
+        managePointOfSaleWidget(view);
 
         pointOfSalesOptionsButton = (Button)view.findViewById(R.id.detailer_point_of_sale);
         pointOfSalesOptions = getResources().getStringArray(R.array.point_of_sale_material);
@@ -104,9 +79,10 @@ public class DetailersActivity extends Fragment {
         Button saveDetailetCallBtn = (Button)view.findViewById(R.id.detailer_submit_btn);
         saveDetailetCallBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                boolean isSaved = saveDetailerCall();
-                if (isSaved) {
+            public void onClick(View view1) {
+                if(!allMandatoryFieldsFilled(view)){
+                    Toast.makeText(getActivity(), "Please fill in all the mandaory fields", Toast.LENGTH_LONG).show();
+                }else if(saveForm()){
                     Toast.makeText(getActivity(), "Detailer Information has been  successfully added!", Toast.LENGTH_LONG).show();
                     Intent i = new Intent(getActivity(), HomeActivity.class);
                     startActivity(i);
@@ -116,7 +92,7 @@ public class DetailersActivity extends Fragment {
             }
         });
         bindDetailerCallToUi(view);
-        managePointOfSaleOthers(view,false);
+        managePointOfSaleOthers(view, false);
         setRequiredFields(view);
         return  view;
     }
@@ -136,156 +112,30 @@ public class DetailersActivity extends Fragment {
         return detailerCall;
     }
 
-    private void initialiseGreenDao() {
+    @Override
+    protected boolean saveForm() {   boolean isSaved = false;
         try {
-            DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(getActivity(), "chai-crm-db", null);
-            db = helper.getWritableDatabase();
-            daoMaster = new DaoMaster(db);
-            daoSession = daoMaster.newSession();
-            villageDao = daoSession.getVillageDao();
-            taskDao = daoSession.getTaskDao();
-            detailerCallDao = daoSession.getDetailerCallDao();
+            bindUiToDetailerCall();
+            detailerCallInstance.setTaskId(callDataTask.getUuid());
+            detailerCallInstance.setTask(callDataTask);
+            if(detailerCallInstance.getIsNew()){
+                detailerCallInstance.setUuid(UUID.randomUUID().toString());
+                detailerCallInstance.setIsNew(false);
+                detailerCallDao.insert(detailerCallInstance);
+            }else{
+                detailerCallDao.update(detailerCallInstance);
+            }
+            callDataTask.setStatus(TaskMainFragment.STATUS_COMPLETE);
+            taskDao.update(callDataTask);
+            isSaved = true;
         } catch (Exception ex) {
-            Log.d("Error=====================================", ex.getLocalizedMessage());
-            Toast.makeText(getActivity(), "Error initialising Database:" + ex.getMessage(), Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
         }
+        return isSaved;
     }
 
-    private void setDateWidget(View view) {
-        Button dateBtn = (Button)view.findViewById(R.id.detailer_survey_date_btn);
-        dateEditTxt = (EditText)view.findViewById(R.id.detailer_survey_date);
-        dateEditTxt.setEnabled(false);
-
-        dateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar calendar = null;
-                String existingDate = (String) dateEditTxt.getText().toString();
-                if (existingDate != null && !existingDate.equals("")) {
-                    StringTokenizer stringTokenizer = new StringTokenizer(existingDate, "/");
-                    initialMonth = stringTokenizer.nextToken();
-                    initialDate = stringTokenizer.nextToken();
-                    initialYear = stringTokenizer.nextToken();
-                    if (datePickerDialog == null) {
-                        datePickerDialog = new DatePickerDialog(view.getContext(), new PickDate(), Integer.parseInt(initialYear),
-                                Integer.parseInt(initialMonth),
-                                Integer.parseInt(initialDate));
-
-                        datePickerDialog.updateDate(Integer.parseInt(initialYear),
-                                Integer.parseInt(initialMonth),
-                                Integer.parseInt(initialDate));
-                    }
-                } else {
-                    calendar = Calendar.getInstance();
-                    if (datePickerDialog == null) {
-                        datePickerDialog = new DatePickerDialog(view.getContext(), new PickDate(), calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-                        datePickerDialog.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-                    }
-                }
-                datePickerDialog.show();
-
-            }
-        });
-    }
-
-    private void setGpsWidget(final View view1) {
-                Button showGps = (Button)view1.findViewById(R.id.detailers_gps_btn);
-                showGps.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                                gpsTracker = new GPSTracker(getActivity());
-                                if (gpsTracker.canGetLocation()) {
-                                        capturedLatitude = gpsTracker.getLatitude();
-                                        capturedLongitude = gpsTracker.getLongitude();
-                                        EditText detailsGps = (EditText)view1.findViewById(R.id.detailers_gps_text);
-                                        detailsGps.setText(capturedLatitude + "," + capturedLongitude);
-                                    } else {
-                                        gpsTracker.showSettingsAlert();
-                                    }
-                            }
-                    });
-            }
-
-    private class PickDate implements DatePickerDialog.OnDateSetListener {
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth) {
-            view.updateDate(year, monthOfYear, dayOfMonth);
-            dateEditTxt.setText(dayOfMonth+ "/"+(monthOfYear+1) + "/" + year);
-            datePickerDialog.hide();
-        }
-    }
-
-    private void manageDoyouStockZincResponses(View view) {
-        final Spinner spinner = (Spinner)view.findViewById(R.id.detailer_do_you_stock_zinc);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                String selected = (String) spinner.getAdapter().getItem(position);
-                LinearLayout stockfieldsLayout = (LinearLayout)getActivity().findViewById(R.id.detailer_zinc_stock_layout);
-                LinearLayout ifnowhyLayout = (LinearLayout)getActivity().findViewById(R.id.detailer_ifnowhy_layout);
-                LinearLayout atWhatPriceDoYouBuyLayout = (LinearLayout)getActivity().findViewById(R.id.detailer_whatpricedoyoubuy_layout);
-                if ("No".equalsIgnoreCase(selected)) {
-                    stockfieldsLayout.setVisibility(View.GONE);
-                    ifnowhyLayout.setVisibility(View.VISIBLE);
-                    atWhatPriceDoYouBuyLayout.setVisibility(View.GONE);
-                } else {
-                    stockfieldsLayout.setVisibility(View.VISIBLE);
-                    ifnowhyLayout.setVisibility(View.GONE);
-                    atWhatPriceDoYouBuyLayout.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-    }
-
-    private void  manageHaveYouHeardAboutDiarheaTreatment(View view){
-        final Spinner spinner = (Spinner)view.findViewById(R.id.detailer_hearabout_treatment_with_zinc_ors);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                String selected = (String) spinner.getAdapter().getItem(position);
-                LinearLayout howdidYouHearAboutTreatment = (LinearLayout)getActivity().findViewById(R.id.detailer_how_did_you_hearabout_zinc_ors_layout);
-                if ("No".equalsIgnoreCase(selected)) {
-                    howdidYouHearAboutTreatment.setVisibility(View.GONE);
-                } else {
-                    howdidYouHearAboutTreatment.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-    }
-
-    private void manageHowDidyouHearOtherOption(View view) {
-        final Spinner spinner = (Spinner)view.findViewById(R.id.detailer_how_did_you_hearabout_zinc_ors);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                String selected = (String) spinner.getAdapter().getItem(position);
-                LinearLayout otherwaysinfoLayout = (LinearLayout)getActivity().findViewById(R.id.detailer_howdidyouhearaboutzinc_other_layout);
-                if ("Other".equalsIgnoreCase(selected)) {
-                    otherwaysinfoLayout.setVisibility(View.VISIBLE);
-                } else {
-                    otherwaysinfoLayout.setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-    }
-
-    private void bindUiToDetailerCall() {
+    @Override
+    protected void bindUiToDetailerCall() {
         detailerCallInstance.setDateOfSurvey(new Date());
         detailerCallInstance.setDiarrheaPatientsInFacility(Integer.parseInt(((EditText)getActivity(). findViewById(R.id.detailer_how_many_diarrhea_patients_in_facility)).getText().toString()));
         detailerCallInstance.setOtherWaysHowYouHeard(((EditText) getActivity().findViewById(R.id.detailer_other_ways_youheard_about_zinc)).getText().toString());
@@ -315,6 +165,11 @@ public class DetailersActivity extends Fragment {
         detailerCallInstance.setKnowledgeAbtZincAndUsage(((Spinner)getActivity(). findViewById(R.id.detailer_how_zinc_should_be_used)).getSelectedItem().toString());
         detailerCallInstance.setLatitude(capturedLatitude);
         detailerCallInstance.setLongitude(capturedLongitude);
+    }
+
+    @Override
+    protected void initDetailerInstance() {
+
     }
 
     private void bindDetailerCallToUi(View view) {
@@ -396,27 +251,6 @@ public class DetailersActivity extends Fragment {
         }
     }
 
-    private boolean saveDetailerCall() {
-        boolean isSaved = false;
-        try {
-            bindUiToDetailerCall();
-            detailerCallInstance.setTaskId(callDataTask.getUuid());
-            detailerCallInstance.setTask(callDataTask);
-            if(detailerCallInstance.getIsNew()){
-                detailerCallInstance.setUuid(UUID.randomUUID().toString());
-                detailerCallInstance.setIsNew(false);
-                detailerCallDao.insert(detailerCallInstance);
-            }else{
-                detailerCallDao.update(detailerCallInstance);
-            }
-            callDataTask.setStatus(TaskMainFragment.STATUS_COMPLETE);
-            taskDao.update(callDataTask);
-            isSaved = true;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return isSaved;
-    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getActivity().getMenuInflater();
@@ -432,81 +266,6 @@ public class DetailersActivity extends Fragment {
                 return true;
             default:
                 return super.onOptionsItemSelected(menuItem);
-        }
-    }
-
-    private void setRequired(TextView textView){
-      String required = "*";
-        SpannableStringBuilder builder = new SpannableStringBuilder();
-        builder.append(textView.getText().toString());
-        int start = builder.length();
-        builder.append(required);
-        int end = builder.length();
-        builder.setSpan(new ForegroundColorSpan(Color.RED),start,end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        textView.setText(builder);
-    }
-
-    private void setRequiredFields(View view){
-        setRequired((TextView)view.findViewById(R.id.detailer_how_many_diarrhea_patients_in_facility_view));
-        setRequired((TextView)view.findViewById(R.id.detailer_hearabout_treatment_with_zinc_ors_view));
-        setRequired((TextView)view.findViewById(R.id.detailer_how_did_you_hearabout_zinc_ors_view));
-        setRequired((TextView)view.findViewById(R.id.detailer_how_diarrhea_affects_community_view));
-        setRequired((TextView)view.findViewById(R.id.detailer_effect_diarrhea_has_on_the_body_view));
-        setRequired((TextView)view.findViewById(R.id.detailer_how_ors_should_be_used_view));
-        setRequired((TextView)view.findViewById(R.id.detailer_how_zinc_should_be_used_view));
-        setRequired((TextView)view.findViewById(R.id.detailer_why_should_not_use_antibiotics_view));
-    }
-
-
-    protected void showPointOfSaleDialog() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
-        dialog.setTitle("Point of Sale Material")
-                .setMultiChoiceItems(pointOfSalesOptions,selections,new DialogSelectionClickHandler())
-                .setPositiveButton("OK", new DialogButtonClickHandler());
-        dialog.show();
-    }
-
-
-    public class DialogButtonClickHandler implements DialogInterface.OnClickListener
-    {
-        public void onClick( DialogInterface dialog, int clicked )
-        {
-            switch( clicked )
-            {
-                case DialogInterface.BUTTON_POSITIVE:
-                    setSelectedOptions();
-                    break;
-            }
-        }
-    }
-
-    public class DialogSelectionClickHandler implements DialogInterface.OnMultiChoiceClickListener
-    {
-        public void onClick( DialogInterface dialog, int clicked, boolean selected )
-        {
-            Log.i( "ME================================", pointOfSalesOptions[clicked] + " selected: " + selected );
-        }
-
-    }
-
-    private void setSelectedOptions() {
-        pointOfSalesOptionsButton.setText("");
-        for( int i = 0; i < pointOfSalesOptions.length; i++ ){
-            Log.i( "ME", pointOfSalesOptions[ i ] + " selected: " + selections[i] );
-            if (selections[i] && !pointOfSalesOptions[i].toString().equalsIgnoreCase("others")) {
-                pointOfSalesOptionsButton.setText((pointOfSalesOptionsButton.getText().toString().equals("")?"":pointOfSalesOptionsButton.getText() + ",") + pointOfSalesOptions[i]);
-            }else  if (selections[i] && pointOfSalesOptions[i].toString().equalsIgnoreCase("others")){
-                managePointOfSaleOthers(getView(),true);
-            }
-        }
-    }
-
-    private void managePointOfSaleOthers(View view,boolean isShow) {
-        LinearLayout pointOfSalesOthersLayout = (LinearLayout)view.findViewById(R.id.detailer_point_of_sale_others_layout);
-        if (isShow) {
-            pointOfSalesOthersLayout.setVisibility(View.VISIBLE);
-        } else {
-            pointOfSalesOthersLayout.setVisibility(View.GONE);
         }
     }
 
