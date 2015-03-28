d118a874e6e9845a03b90b14fdccd966790fb0e7
Victor kakama <vkakama@gmail.com>
2015-03-28 14:48:42 +0300

    return right error

--- src/main/java/org/chai/rest/CustomerClient.java
+++ src/main/java/org/chai/rest/CustomerClient.java
@@ -32,7 +32,7 @@ public class CustomerClient extends RestClient {
             ResponseEntity<ServerResponse> responseEntity = restTemplate.exchange(REST_URL + "customer/update", HttpMethod.PUT, httpEntity, ServerResponse.class);
             Log.i("Rest Customer post Response:", "==============================================================================" + responseEntity.getBody().getMessage());
 
-            return new ServerResponse("200", "Successfully uploaded Customers");
+            return responseEntity.getBody();
         } catch (HttpClientErrorException ex) {
             ServerResponse serverResponse = ServerResponse.getServerErrorResponse(ex);
             return serverResponse;
